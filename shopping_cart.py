f8d8c05240e0787895116835c4882ca4936f3abb
liza <hama0104@algonquinlive.com>
2025-01-27 16:24:59 -0500

    Fix the duplicated errors by creating a function called shop().

--- shopping_cart.py
+++ shopping_cart.py
@@ -7,28 +7,53 @@ Users can add items, specify quantities, and see the total cost.
 The program uses exception handling to manage invalid inputs.
 
 """
-from logging import exception
+def shop():
 
-price = []
-name = []
-qnt = []
-try:
-    print("Welcome to the Shopping Cart Program!")
+    price = []
+    name = []
+    qnt = []
+    total_cost=0
 
-    for i in range(3):
-        iname = (input("Enter the item name :"))
-        name.append(iname)
-        pitem=int(input(f"Enter the number of item {name[i]}:"))
-        price.append(pitem)
-        iqnt=int(input(f"Enter the quantity {name[i]} :"))
-        qnt.append(iqnt)
+    try:
+        print("Welcome to the Shopping Cart Program!")
 
-    total_cost=price[i]*qnt[i]
-    print(f"The total cost is: {total_cost}")
+        for i in range(3):
+            i_name = (input("Enter the item name :"))
+            name.append(i_name)
+            p_item=int(input(f"Enter the price of item {name[i]}:"))
+            price.append(p_item)
+            i_qnt=int(input(f"Enter the quantity {name[i]} :"))
+            qnt.append(i_qnt)
+
+          # total_cost=price[i]*qnt[i]
+          #   print(f"The total cost is: {total_cost}")
 
 
 
-except :
-    print('Invalid input')
+    except ValueError:
+        print('Invalid input')
 
-#############
+    #############
+    #total_cost=0
+
+    for i in range(len(price)):
+        total_cost = price[i] * qnt[i] + total_cost
+
+    print(total_cost)
+    if total_cost > 100:
+        discount = total_cost * 0.1
+        total_cost -= discount
+        print(f"\nYou saved ${discount:.2f} with a 10% discount!")
+        print(f"Discounted Total: ${total_cost:.2f}")
+
+while True:
+    restart = input("\nWould you like to shop again? (yes/no): ").lower()
+    if restart == "yes":
+        shop()
+
+    else:
+     print("Thank you for shopping with us!")
+
+
+
+
